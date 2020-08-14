fc6178e6e58326131ea4d8b53bf3df4d4a73c852
Seibeaaar <yuribar4520@gmail.com>
2020-08-14 18:49:50 +0300

    Updates

---
+++ precache-manifest.f8ec904615b1d8a52d736581a8ace858.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dda9f905806e8418de9454170e43ff",
+    "url": "/Meteor-News/index.html"
+  },
+  {
+    "revision": "56047fcb703bdf359166",
+    "url": "/Meteor-News/static/css/main.67fce65c.chunk.css"
+  },
+  {
+    "revision": "e597bd4d1213123e0893",
+    "url": "/Meteor-News/static/js/2.c9ada37f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Meteor-News/static/js/2.c9ada37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdb330030af8699469f",
+    "url": "/Meteor-News/static/js/3.327dbd69.chunk.js"
+  },
+  {
+    "revision": "36b4c072943dfed4f74b",
+    "url": "/Meteor-News/static/js/4.ddf2fd2e.chunk.js"
+  },
+  {
+    "revision": "56047fcb703bdf359166",
+    "url": "/Meteor-News/static/js/main.225df249.chunk.js"
+  },
+  {
+    "revision": "f5e6c1ff365f92614e32",
+    "url": "/Meteor-News/static/js/runtime-main.d3d62bab.js"
+  },
+  {
+    "revision": "defc129761cd0670ef2a278ffa1c5305",
+    "url": "/Meteor-News/static/media/NotFound.defc1297.jpg"
+  }
+]);
